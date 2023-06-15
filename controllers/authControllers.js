523315617005acd502ae3b4e02b5903fe0dd5f3f
[Yel Yan] <[kmin2957@gmail.com]>
2023-06-15 21:34:47 +0300

    validation for email & password

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,29 @@
 const User = require("../models/userModel");
 
-// controller action 
+// handle err for email & password
+function handleErrors(err){
+    console.log(err.message, err.code);
+    let errors = {email : "", password : ""}; 
 
+    // duplicate email error
+    if(err.code === 11000){
+        errors.email = "This email is already registered"
+        return errors;
+    };
+
+    // validation errors
+    if(err.message.includes("User validation failed")){
+        Object.values(err.errors).forEach(x => {
+            let { properties} = x;
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors;
+}
+
+
+
+// controller action 
 const signup_get = async (req, res) =>{
     try {
         const allUsers = await User.find({});
@@ -23,11 +45,11 @@ const signup_post =  async (req, res) => {
         const user = await User.create({ email, password}); // create & save in database
         return  res.status(201).json(user);
     } catch (err) {
-        console.log(err)
-       return res.status(400).send("Error, user not created");
+       const errors =  handleErrors(err);
+       return res.status(400).json(errors);
     }
-
 }
+
 
 const login_post = (req, res) => {
     const {email, password} = req.body;
--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,29 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const {isEmail} = require('validator');
 
 const UserSchema = new Schema({
     email : {
         type : String,
-        required  : true,
+        required  : [true, "Please enter an email"],
         unique : true,
-        lowercase : true
+        lowercase : true,
+        validate : [isEmail, "Please enter a valid email"]
     }, 
     password : {
         type : String,
-        required : true,
-        minLength : [6, "characters must be at least 6 words"]
+        required : [true, "Please enter a password"],
+        minlength : [6, "Minimum password length is 6 characters"]
     }
 });
 
 
-const User = mongoose.model("User", UserSchema);
 
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
+
+/*
+What is the validate function val argument come from?
+It is from submit form when user click submit btn and email will pass as an argument.
+If it returns true, it's a valid email & if it return false we thorw error
+*/
